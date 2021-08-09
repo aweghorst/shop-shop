2b029491640782c58b7e029e3a3175299dce6c4e
aw2389 <alex2@weghorst.com>
2021-08-09 12:31:54 -0500

    deploy to heroku

---
+++ client/build/precache-manifest.a39cd1bbfaee61ccb6d141246dd5baa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ffabb3ec20c0ba204025e7ddd6dea1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c73293d2b59150c3dede",
+    "url": "/static/css/main.c00387be.chunk.css"
+  },
+  {
+    "revision": "af036c7b6156a7df0966",
+    "url": "/static/js/2.4ef3d930.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.4ef3d930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c73293d2b59150c3dede",
+    "url": "/static/js/main.c6cfd05a.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
